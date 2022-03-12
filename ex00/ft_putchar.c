5500c1b5099bbca5b067108c3cb2a652290b71bf
Muja Mo <muja@foss.land>
2022-03-12 17:52:11 +0100

    added ex00 ft_putchar

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,8 +6,19 @@
 /*   By: mujmoham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 16:17:13 by mujmoham          #+#    #+#             */
-/*   Updated: 2022/03/10 16:17:29 by mujmoham         ###   ########.fr       */
+/*   Updated: 2022/03/12 17:42:03 by mujmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>                   //call standard library
+    
+void ft_putchar(char c)               //declare ft_putchar function
+{
+    write(1, &c, 1);
+};
 
+int main()                           // declare main function
+{
+    ft_putchar('4');                 // call ft_putchar function
+    return 0;
+};
