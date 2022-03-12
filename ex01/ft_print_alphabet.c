855dd0118cc6ed9571058cd62719e8d73abe023e
Muja Mo <muja@foss.land>
2022-03-12 20:33:41 +0100

    added ex01 ft_print_alphabet

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -5,9 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mujmoham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/12 19:07:43 by mujmoham          #+#    #+#             */
-/*   Updated: 2022/03/12 19:07:49 by mujmoham         ###   ########.fr       */
+/*   Created: 2022/03/12 19:48:26 by mujmoham          #+#    #+#             */
+/*   Updated: 2022/03/12 20:31:22 by mujmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+void    ft_putchar(char a)                      // using ft_putchar function ex00
+{
+    write(1, &a, 1);
+}
+
+void    ft_print_alphabet(void)                 // declare alphabet function 
+{
+    char    l;
+
+    l = 'a';
+    while (l <= 'z')
+    {
+        ft_putchar(l);
+        l++;                                    
+    }
+};
+
+int     main(void)                              // declare main function
+{   
+    ft_print_alphabet();
+    return 0;
+};
