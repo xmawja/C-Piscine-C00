99097f26d9838b48828bc690b21e2a0d2b8af392
Muja Mo <muja@foss.land>
2022-03-14 13:35:28 +0100

    fix ex05 ft_print_comb

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,15 +6,15 @@
 /*   By: mujmoham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 09:03:27 by mujmoham          #+#    #+#             */
-/*   Updated: 2022/03/14 09:25:41 by mujmoham         ###   ########.fr       */
+/*   Updated: 2022/03/14 12:12:48 by mujmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(int n)
+void ft_putchar(char n)
 {
-    write(1, &n, 3);
+    write(1, &n, 1);
 };
 
 void ft_print_comb(void)
@@ -22,21 +22,30 @@ void ft_print_comb(void)
     int a;
     int b;
     int c;
-    for(a = '0'; a <= '9'; ++a)
+    a = 0;
+    while (a <= 9)
     {
-        for(b = '0'; b <= '9'; ++b)
+        b = a+1;
+        while (b <= 9)
         {
-            for(c = '0'; c <='9'; ++c)
+            c =b+1;
+            while (c <= 9)
             {
-                ft_putchar(a);
-                ft_putchar(b);
-                ft_putchar(c);
-                ft_putchar(',');
-                ft_putchar(' ');
-            };
-        };
-    };
-};
+                ft_putchar(a + '0');
+                ft_putchar(b + '0');
+                ft_putchar(c + '0');
+				c++;
+				if(a != 7)
+					{
+						ft_putchar(',');
+						ft_putchar(' ');
+					}
+            }
+            b++;
+        }
+        a++;
+    }
+}
 
 int main()
 {
