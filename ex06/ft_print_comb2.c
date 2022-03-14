c73b02002a60011c8f29a8cefee1aad6b302a4ef
Muja Mo <muja@foss.land>
2022-03-14 13:56:43 +0100

    added ex06 ft_print_comb2

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -11,3 +11,38 @@
 /* ************************************************************************** */
 
 
+#include <unistd.h>
+
+void ft_putchar(char n) 
+{
+	write(1, &n, 1);
+}
+
+void ft_print_comb2(void)
+{
+	int a;
+	int b;
+	a = 0;
+	while (a <= 9)
+		{
+			a = 0;
+			while (b <= 9)
+				{
+					b = 0;
+					ft_putchar(a);
+					ft_putchar(b);
+					b++;
+					if (a <= 9) {
+						ft_putchar(',');
+						ft_putchar(' ');
+					}
+				}
+			a++;
+		}
+}
+
+int main()
+{
+	ft_print_comb2();
+	return 0;
+}
