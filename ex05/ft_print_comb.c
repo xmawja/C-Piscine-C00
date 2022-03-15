fd8d0dd91eb11f9e0e4f091ab7cf30c06749bfa5
Muja Mo <muja@foss.land>
2022-03-15 14:36:40 +0100

    fixx norminette syntax checker script

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,49 +6,47 @@
 /*   By: mujmoham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 09:03:27 by mujmoham          #+#    #+#             */
-/*   Updated: 2022/03/14 12:12:48 by mujmoham         ###   ########.fr       */
+/*   Updated: 2022/03/15 14:35:59 by mujmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char n)
+void	ft_putchar(char n)
 {
-    write(1, &n, 1);
-};
-
-void ft_print_comb(void)
-{
-    int a;
-    int b;
-    int c;
-    a = 0;
-    while (a <= 9)
-    {
-        b = a+1;
-        while (b <= 9)
-        {
-            c =b+1;
-            while (c <= 9)
-            {
-                ft_putchar(a + '0');
-                ft_putchar(b + '0');
-                ft_putchar(c + '0');
-				c++;
-				if(a != 7)
-					{
-						ft_putchar(',');
-						ft_putchar(' ');
-					}
-            }
-            b++;
-        }
-        a++;
-    }
+	write(1, &n, 1);
 }
 
-int main()
+void	ft_print_comb(void)
 {
-    ft_print_comb();
-    return 0;
-};
+	char	a;
+	char	b;
+	char	c;
+
+	a = 48;
+	while (a < 56)
+	{
+		b = a + 1;
+		while (b > a && b < 57)
+		{
+			c = b + 1;
+			while (c > b && c <= 57)
+			{
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
+				if (!(a == 55))
+					write(1, ",", 1);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
