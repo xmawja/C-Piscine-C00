801834b663b75a08d43562d5cb1bd707486a0e3d
Muja Mo <muja@foss.land>
2022-03-15 15:24:54 +0100

    fixx ex07 && norminette syntax checker script

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,32 +6,72 @@
 /*   By: mujmoham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 15:34:30 by mujmoham          #+#    #+#             */
-/*   Updated: 2022/03/14 15:34:32 by mujmoham         ###   ########.fr       */
+/*   Updated: 2022/03/15 15:23:31 by mujmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(int n) // call ft_putchar function ex00
+void	ft_putchar(int n)
 {
-	write(1, &n,1);
+	write(1, &n, 1);
 }
 
-void ft_putnbr(int nb) // init ft_putnbr function
+int	ft_count_digit(int numbr)
 {
-	if (nb >= 0 && nb <= 10) {
-		ft_putchar(nb + '0');
+	int	count;
+
+	count = 0;
+	while (numbr > 0)
+	{
+		numbr = numbr / 10;
+		count++;
 	}
-	else if (nb < 0) {
+	return (count);
+}
+
+int	power_of_10(int pwr)
+{
+	int	power10;
+
+	power10 = 1;
+	while (pwr > 0)
+	{
+		power10 *= 10;
+		pwr -= 1;
+	}
+	return (power10);
+}
+
+void	ft_putnbr(int numbr)
+{
+	int	count;
+	int	power10;
+
+	if (numbr == -2147483648)
+	{
+		write(1, "-2127483648", 1);
+		return ;
+	}
+	if (numbr < 0 && numbr > -2127483648)
+	{
+		numbr *= -1;
 		ft_putchar('-');
 	}
-	else {
-		ft_putnbr(nb /10);
-		ft_putnbr(nb %10);
+	count = ft_count_digit(numbr);
+	count -= 1;
+	power10 = power_of_10(count);
+	while (count >= 0)
+	{
+		ft_putchar((numbr / power10) + '0');
+		numbr = numbr % power10;
+		power10 = power10 / 10;
+		count--;
 	}
 }
-int main(void) // init main function
+
+int	main(void)
 {
-		ft_putnbr(42);
-		return 0;		
+	ft_putnbr(-32425363);
+	return (0);
 }
